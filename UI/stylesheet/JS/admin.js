3036ffdb9f62f4d3d68c1a29c8f15e1a94908505
MajemiteJames <majemiteokoro@gmail.com>
2018-12-05 12:08:35 +0100

    chore(admin):created an admin page - https://www.pivotaltracker.com/n/projects/2226625 - where the admin can login into his page [Delivers #162442467]

---
+++ UI/stylesheet/JS/admin.js
@@ -0,0 +1,34 @@
+function ValidateForm() {
+    const username = document.getElementById('username');
+    const phoneNum = document.getElementById('phoneNum');
+    const password = document.getElementById('password');
+    const confirmPassword = document.getElementById('confirm_password');
+    let valid = true;
+    if (username.value.length === 0) {
+      username.className = 'wrong-input';
+      username.nextElementSibling.innerHTML = "Username can't be blank";
+      valid = false;
+    }
+    if (password.value.length < 6) {
+      password.className = 'wrong-input';
+      password.nextElementSibling.innerHTML = 'Password cannot be less than 6';
+      valid = false;
+    }
+    if (password.value !== confirmPassword.value) {
+      confirmPassword.className = 'wrong-input';
+      confirmPassword.nextElementSibling.innerHTML = 'Password does not match';
+      valid = false;
+    }
+    return valid;
+  }
+  function removeMessage() {
+    const errorinput = document.querySelectorAll('wrong-input'); 
+    [].forEach.call(errorinput, function (el) {
+    el.classList.remove('wrong-input'); 
+   });
+    const errorpara = document.querySelectorAll('.error'); 
+    [].forEach.call(errorpara, function(el){
+    el.innerHTML="";
+    });
+    }  
+  removeMessage();
